af838df7bf5a268ec6143fa2def8b2be49e2780b
kiran <kirankiru3434@gmail.com>
2023-05-22 17:05:53 +0530

    added mail-service and logger-service to log the events

---
+++ logger-service/cmd/api/grpc.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"log-service/data"
+	"log-service/logs"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+// this will be the server
+type LogServer struct {
+	logs.UnimplementedLogServiceServer
+	//this is required for all the service which we write over grpc
+	//to ensure backwards compatibility
+	Models data.Models
+}
+
+func (l *LogServer) WriteLog(ctx context.Context, req *logs.LogRequest) (*logs.LogResponse, error) {
+	input := req.GetLogEntry()
+	//write the log
+	logEntry := data.LogEntry{
+		Name: input.Name,
+		Data: input.Data,
+	}
+	//write this to mongo
+	err := l.Models.LogEntry.Insert(logEntry)
+	if err != nil {
+		res := &logs.LogResponse{Result: "failed"}
+		return res, err
+
+	}
+	//return response
+	res := &logs.LogResponse{Result: "logged!"}
+	return res, nil
+}
+
+// function to grpclistener
+func (app *Config) gRpcListen() {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%s", gRpcPort))
+	if err != nil {
+		log.Fatalf("Falied to listen to grpc: %w", err)
+	}
+
+	s := grpc.NewServer()
+	//register the server
+	logs.RegisterLogServiceServer(s, &LogServer{Models: app.Models})
+	log.Printf("grpc server started on the port %s", gRpcPort)
+	if err := s.Serve(lis); err != nil {
+		log.Fatal("Failed to listen for grpc: %w", err)
+	}
+
+}
